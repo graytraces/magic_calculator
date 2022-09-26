14bd857ff910f3b06ec9e4a85d50b86e1b8f7ced
graytraces <graytraces@gmail.com>
2022-09-27 06:22:22 +0900

    최적질문 구하는 로직 추가

--- magic_calculator_app/lib/number_analysis/question_helper.dart
+++ magic_calculator_app/lib/number_analysis/question_helper.dart
@@ -1,19 +1,20 @@
+import 'number_statistics.dart';
+
 enum QuestionCandidate {
   black1,
   black9,
   black11,
   black19,
-  green,  //앞이 커요?
-  red1,   //odd
+  green, //앞이 커요?
+  red1, //odd
   red9,
   red11,
   red19,
-  blue1,  //gt4
+  blue1, //gt4
   blue9,
   blue11,
   blue19
 }
-
 
 class QuestionCase {
   List<QuestionCandidate> questionList;
@@ -35,5 +36,139 @@ class QuestionCase {
 
   void setWorstCount(int worstCount) {
     this.worstCount = worstCount;
+  }
+}
+
+class QuestionMaker {
+  late int modifier;
+  late int matrixSize;
+  late NumberStatistics numberStatistics;
+
+  QuestionMaker(List<List<int>> pairList) {
+    this.modifier = QuestionCandidate.green.index;
+    this.matrixSize = QuestionCandidate.values.length - modifier;
+
+    NumberStatistics numberStatistics = new NumberStatistics(pairList.length);
+    numberStatistics.makeStatistics(pairList, 4, 4);
+  }
+
+  List<QuestionCase> getBestQuestionSet(List<QuestionCase> resultList){
+    resultList.sort( (a, b) => a.worstCount - b.worstCount);
+    List<QuestionCase> bestResult = List<QuestionCase>.from(resultList.where((questionCase) => questionCase.worstCount == 1));
+    bestResult.sort((a, b) => a.questionList.length - b.questionList.length);
+    return bestResult;
+  }
+
+  getFirstDepthQuestionCase(List<QuestionCase> resultList) {
+    for (int i = 0; i < matrixSize; i++) {
+      List<QuestionCandidate> candidateList = [];
+      QuestionCandidate questionCandidate =
+          QuestionCandidate.values[i + modifier];
+      candidateList.add(questionCandidate);
+      int maxValue = numberStatistics.makeOptimalQuestion(candidateList);
+      //resultMap.put(candidateList, maxValue);
+      resultList.add(new QuestionCase(candidateList, maxValue));
+    }
+  }
+
+  getSecondDepthQuestionCase(List<QuestionCase> resultList) {
+    for (int i = 0; i < matrixSize; i++) {
+      List<QuestionCandidate> candidateList = [];
+      QuestionCandidate questionCandidate =
+          QuestionCandidate.values[i + modifier];
+      candidateList.add(questionCandidate);
+      for (int j = 0; j < matrixSize; j++) {
+        QuestionCandidate questionCandidate2 =
+            QuestionCandidate.values[j + modifier];
+        if (candidateList.contains(questionCandidate2)) {
+          continue;
+        }
+        List<QuestionCandidate> candidateList2 =
+            List<QuestionCandidate>.from(candidateList);
+        candidateList2.add(questionCandidate2);
+        int maxValue = numberStatistics.makeOptimalQuestion(candidateList2);
+        //resultMap.put(candidateList2, maxValue);
+        resultList.add(QuestionCase(candidateList2, maxValue));
+      }
+    }
+  }
+
+  getThirdDepthQuestionCase(List<QuestionCase> resultList) {
+    for (int i = 0; i < matrixSize; i++) {
+      List<QuestionCandidate> candidateList = [];
+      QuestionCandidate questionCandidate =
+          QuestionCandidate.values[i + modifier];
+      candidateList.add(questionCandidate);
+      for (int j = 0; j < matrixSize; j++) {
+        QuestionCandidate questionCandidate2 =
+            QuestionCandidate.values[j + modifier];
+        if (candidateList.contains(questionCandidate2)) {
+          continue;
+        }
+        List<QuestionCandidate> candidateList2 =
+            List<QuestionCandidate>.from(candidateList);
+        candidateList2.add(questionCandidate2);
+
+        for (int k = 0; k < matrixSize; k++) {
+          QuestionCandidate questionCandidate3 =
+              QuestionCandidate.values[k + modifier];
+          if (candidateList2.contains(questionCandidate3)) {
+            continue;
+          }
+          List<QuestionCandidate> candidateList3 =
+              List<QuestionCandidate>.from(candidateList2);
+          candidateList3.add(questionCandidate3);
+
+          int maxValue = numberStatistics.makeOptimalQuestion(candidateList3);
+          //resultMap.put(candidateList3, maxValue);
+          resultList.add(new QuestionCase(candidateList3, maxValue));
+        }
+      }
+    }
+  }
+
+  getFourthDepthQuestionCase(List<QuestionCase> resultList) {
+    for (int i = 0; i < matrixSize; i++) {
+      List<QuestionCandidate> candidateList = [];
+      QuestionCandidate questionCandidate =
+          QuestionCandidate.values[i + modifier];
+      candidateList.add(questionCandidate);
+      for (int j = 0; j < matrixSize; j++) {
+        QuestionCandidate questionCandidate2 =
+            QuestionCandidate.values[j + modifier];
+        if (candidateList.contains(questionCandidate2)) {
+          continue;
+        }
+        List<QuestionCandidate> candidateList2 =
+            List<QuestionCandidate>.from(candidateList);
+        candidateList2.add(questionCandidate2);
+
+        for (int k = 0; k < matrixSize; k++) {
+          QuestionCandidate questionCandidate3 =
+              QuestionCandidate.values[k + modifier];
+          if (candidateList2.contains(questionCandidate3)) {
+            continue;
+          }
+          List<QuestionCandidate> candidateList3 =
+              List<QuestionCandidate>.from(candidateList2);
+          candidateList3.add(questionCandidate3);
+
+          for (int l = 0; l < matrixSize; l++) {
+            QuestionCandidate questionCandidate4 =
+                QuestionCandidate.values[l + modifier];
+            if (candidateList3.contains(questionCandidate4)) {
+              continue;
+            }
+            List<QuestionCandidate> candidateList4 =
+                List<QuestionCandidate>.from(candidateList3);
+            candidateList4.add(questionCandidate4);
+
+            int maxValue = numberStatistics.makeOptimalQuestion(candidateList4);
+            //resultMap.put(candidateList4, maxValue);
+            resultList.add(new QuestionCase(candidateList4, maxValue));
+          }
+        }
+      }
+    }
   }
 }
