c975d3b4f771607fbf6c4b0233c7337f5371dd30
graytraces <graytraces@gmail.com>
2023-04-07 21:23:56 +0900

    구버전 포팅

---
+++ magic_calculator_app/lib/screens/case_calculator_screen_old.dart
@@ -0,0 +1,485 @@
+import 'package:flutter/material.dart';
+import 'package:magic_calculator_app/app_stat_provider.dart';
+import 'package:magic_calculator_app/number_analysis/question_helper.dart';
+import 'package:magic_calculator_app/widgets/main/explain_result.dart';
+import 'package:magic_calculator_app/widgets/main/explain_text.dart';
+import 'package:provider/provider.dart';
+
+class CaseCalculatorScreenOld extends StatefulWidget {
+  const CaseCalculatorScreenOld({Key? key}) : super(key: key);
+
+  @override
+  _CaseCalculatorScreenOldState createState() => _CaseCalculatorScreenOldState();
+}
+
+class _CaseCalculatorScreenOldState extends State<CaseCalculatorScreenOld> {
+  final double _verticalPaddingBetweenWidget = 20;
+
+  TextEditingController inputNumberController = TextEditingController();
+
+  String _resultMessage = "";
+  String _firstNumberLength = "4";
+  String _secondNumberLength = "4";
+  bool _isUseBlackQuestion = true;
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    inputNumberController.dispose();
+  }
+
+  _getContentTextStyle() {
+    return const TextStyle(fontSize: 16);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    AppStatProvider appStatProvider = context.read<AppStatProvider>();
+
+    return Scaffold(
+      appBar: AppBar(title: Text('경우의 수 계산기')),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: _verticalPaddingBetweenWidget,
+              ),
+              TextField(
+                autofocus: true,
+                controller: inputNumberController,
+                keyboardType: TextInputType.number,
+                minLines: 1,
+                maxLines: 1,
+              ),
+              SizedBox(
+                height: _verticalPaddingBetweenWidget,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {
+                      getFirstQuestion();
+                    },
+                    child: Text('계산하기')),
+              ),
+              SizedBox(
+                height: _verticalPaddingBetweenWidget,
+              ),
+              _strNumberPairList.length == 0
+                  ? SizedBox(
+                  width: double.infinity,
+                  height: 40,
+                  child: Center(
+                      child: Column(
+                        children: [
+                          Text(_resultMessage, style: _getAlertTextStyle()),
+                        ],
+                      )))
+                  : SizedBox(
+                  width: double.infinity,
+                  height: 20,
+                  child: Text(
+                      "○ 경우의수 : " +
+                          _strNumberPairList.length.toString() +
+                          " 가지",
+                      style: _getContentTextStyle())),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: drawOptimalQuestion(),
+                ),
+              ),
+              ExplainResult(_strFilteredNumberPairList, appStatProvider),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  List<List<String>> _strNumberPairList = [];
+  List<List<String>> _strFilteredNumberPairList = [];
+
+  List<List<int>> _intNumberPairList = [];
+  List<QuestionCase> _bestQuestionSet = [];
+
+  QuestionCase _bestQuestion = QuestionCase([], 0, 0);
+  List<String> _answerList = [];
+  QuestionMaker _questionMaker = QuestionMaker([]);
+
+
+  bool isInt(String inputStr) {
+    if (inputStr == null) {
+      return false;
+    }
+    return int.tryParse(inputStr) != null;
+  }
+
+  //숫자 계산
+  void calculateNumber(String inputText) {
+    _strNumberPairList = []; //입력값에 대한 pair String ver
+    _intNumberPairList = []; //입력값에 대한 pair int ver
+
+    _strFilteredNumberPairList = []; //filter 결과값
+
+    _bestQuestionSet = []; //최종 베스트답
+    _bestQuestion = QuestionCase([], 0, 0);
+
+    _answerList = []; //입력한 답 담는곳
+
+    if (!isInt(inputText)) {
+      _resultMessage = "정수 숫자를 입력해주세요";
+      return;
+    }
+
+    var inputNumber = int.parse(inputText);
+
+    int startNumber = 2000;
+    int endNumber = 9999;
+    // for (int idx = 1; idx < int.parse(_firstNumberLength); idx++) {
+    //   //startNumber *= 10;
+    //   endNumber = endNumber * 10 + 9;
+    // }
+
+    String strFirstNumber = "";
+    String strSecondNumber = "";
+
+    for (int firstNumber = startNumber; firstNumber <= endNumber; firstNumber++) {
+      //if (inputNumber % (100000 + firstNumber) == 0) {
+      if (inputNumber % (firstNumber) == 0) {
+        var secondNumber = (inputNumber / firstNumber).toInt();
+
+        //if (firstNumber.toString().length > int.parse(_firstNumberLength) + 2 ||
+        if (firstNumber
+            .toString()
+            .length > int.parse(_firstNumberLength) ||
+            secondNumber
+                .toString()
+                .length > int.parse(_secondNumberLength)) {
+          continue;
+        }
+
+        strFirstNumber = firstNumber.toString();
+        if (strFirstNumber.length < int.parse(_firstNumberLength)) {
+          for (int idx2 = strFirstNumber.length; idx2 < int.parse(_firstNumberLength); idx2++) {
+            strFirstNumber = "0" + strFirstNumber;
+          }
+        }
+
+        strSecondNumber = secondNumber.toString();
+
+        if (strSecondNumber.length < int.parse(_secondNumberLength)) {
+          for (int idx2 = strSecondNumber.length; idx2 < int.parse(_secondNumberLength); idx2++) {
+            strSecondNumber = "0" + strSecondNumber;
+          }
+        }
+
+        _strNumberPairList.add([strFirstNumber, strSecondNumber]);
+        _intNumberPairList.add([firstNumber, secondNumber]);
+      }
+    }
+
+    if (_strNumberPairList.isEmpty) {
+      setState(() {
+        _resultMessage = "계산결과값이 없습니다";
+      });
+
+      return;
+    }
+
+    List<QuestionCase> resultList = [];
+    _questionMaker = QuestionMaker(_intNumberPairList);
+
+    QuestionCandidate? blackOneShotQuestion;
+    if (_isUseBlackQuestion) {
+      blackOneShotQuestion = _questionMaker.getBlackOneShotQuestion();
+    }
+
+    QuestionCase blackBestQuestion = QuestionCase([], 0, 0);
+    List<QuestionCase> blackBestQuestionSet = [];
+
+    if (blackOneShotQuestion != null) {
+      List<QuestionCandidate> blackOneshotQuestionCandidateList = [];
+      List<QuestionCase> blackOneshotQuestionCaseList = [];
+      blackOneshotQuestionCandidateList.add(blackOneShotQuestion);
+      blackBestQuestion = QuestionCase(blackOneshotQuestionCandidateList, 1, 1);
+      blackOneshotQuestionCaseList.add(blackBestQuestion);
+      blackBestQuestionSet = blackOneshotQuestionCaseList;
+    }
+
+    bool findFirstStep = false;
+
+    for (int i = 0; i < 4; i++) {
+      //resultList = 모든 케이스를 넣는다.
+      _questionMaker.getQuestionCase(resultList, _isUseBlackQuestion); //실제 답 찾는 로직
+      _bestQuestionSet = _questionMaker.getBestQuestionSet(
+          resultList, 1);
+
+      if (_bestQuestionSet.isNotEmpty) {
+        if (i == 0) {
+          findFirstStep = true;
+        }
+        break;
+      } else if (i == 0) {
+        if (blackOneShotQuestion != null) {
+          // 한방에 답 찾은경우
+          setState(() {
+            _bestQuestion = blackBestQuestion;
+            _bestQuestionSet = blackBestQuestionSet;
+          });
+          break;
+        }
+      }
+    }
+
+    for (QuestionCase qCase in _bestQuestionSet) {
+      qCase.questionList.sort((a, b) => a.index - b.index);
+    }
+
+    if (_bestQuestionSet.isEmpty) {
+      _bestQuestionSet.add(resultList[0]);
+      setState(() {
+        _bestQuestionSet[0].questionList.sort((a, b) => a.index - b.index);
+      });
+    } else {
+      //한방에 찾았고, black을 안쓸수 있는지 확인해본다.
+      if (findFirstStep) {
+        QuestionCase qCase = _bestQuestionSet[_bestQuestionSet.length - 1];
+
+        if (!qCase.questionList[0].name.contains("black")) {
+          //black 외에 질문이 존재함
+
+          for (int i = 0; i < _bestQuestionSet.length; i++) {
+            QuestionCase qCase2 = _bestQuestionSet[i];
+            if (qCase2.questionList[0].name.contains("black")) {
+              _bestQuestionSet.remove(qCase2);
+              i--;
+            }
+          }
+        }
+      }
+    }
+
+    setState(() {
+      _bestQuestion = _bestQuestionSet[0];
+      _answerList = List.filled(_bestQuestion.questionList.length, "false");
+    });
+  }
+
+  int getFirstQuestion() {
+
+    String number = inputNumberController.text;
+    //계산하기 버튼에 의해서 트리거 된다.
+    calculateNumber(number);
+
+    //경우의 수 계산 + 질문추천1번 만들기
+    return 10;
+  }
+
+  int getSecondQuestion() {
+    //첫번째 질문의 답에 의해서 트리거된다.
+
+    //filtered array를 넣고 최적값을 구해야하네...
+    return 10;
+  }
+
+  int getThirdQuestion() {
+    //두번째 질문의 답에 의해서 트리거된다.
+    return 10;
+  }
+
+  int getFourthQuestion() {
+    //세번째 질문의 답에 의해서 트리거 된다.
+    return 10;
+  }
+
+  int getFifthQuestion() {
+    //네번째 질문의 답에 의해서 트리거 된다.
+    return 10;
+  }
+
+
+
+
+
+
+  _getAlertTextStyle() {
+    return const TextStyle(
+        fontSize: 20,
+        backgroundColor: Colors.yellowAccent,
+        color: Colors.red,
+        fontWeight: FontWeight.bold);
+  }
+
+  drawOptimalQuestion() {
+    var blackAnswerList = <String>['false', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    return _bestQuestionSet.isEmpty
+        ? SizedBox(
+        width: double.infinity,
+        height: 90,
+        child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text("최적질문이 없습니다", style: _getAlertTextStyle()),
+                ),
+              ],
+            )))
+        : SizedBox(
+      width: double.infinity,
+      height: (90 * _bestQuestion.questionList.length).toDouble(),
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ExplainText(_bestQuestion.questionList[index].name),
+                ),
+                Center(
+                  child: _bestQuestion.questionList[index].name.contains("black")
+                      ? SizedBox(
+                    width: 100,
+                    height: 40,
+                    child: DropdownButton(
+                      isExpanded: true,
+                      items:
+                      blackAnswerList.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Center(child: Text(value == "false" ? "선택" : value)),
+                        );
+                      }).toList(),
+                      value: _answerList[index],
+                      onChanged: (String? newValue) async {
+                        setState(() {
+                          _answerList[index] = newValue!;
+                        });
+                        _applyFilter();
+                      },
+                    ),
+                  )
+                      : ToggleButtons(
+                    direction: Axis.horizontal,
+                    onPressed: (int toggleIndex) {
+                      setState(() {
+                        // The button that is tapped is set to true, and the others to false.
+                        _answerList[index] = (0 == toggleIndex).toString();
+                      });
+                      _applyFilter();
+                    },
+                    selectedColor: Colors.white,
+                    fillColor: Colors.white24,
+                    color: Colors.white24,
+                    constraints: const BoxConstraints(
+                      minHeight: 30.0,
+                      minWidth: 60.0,
+                    ),
+                    isSelected: [
+                      _answerList[index].toLowerCase() == "true",
+                      _answerList[index].toLowerCase() == "false"
+                    ],
+                    children:
+                    getToggleButtonTextList(_bestQuestion.questionList[index].name),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: _bestQuestion.questionList.length,
+      ),
+    );
+  }
+
+  getToggleButtonTextList(String name) {
+    List<Widget> questionAnswers = [];
+
+    if (name.contains("red")) {
+      questionAnswers = <Widget>[
+        Text(
+          '홀',
+          style: _getContentTextStyle(),
+        ),
+        Text(
+          '짝',
+          style: _getContentTextStyle(),
+        )
+      ];
+    } else if (name.contains("blue")) {
+      questionAnswers = <Widget>[
+        Text(
+          '0~4',
+          style: _getContentTextStyle(),
+        ),
+        Text(
+          '5~9',
+          style: _getContentTextStyle(),
+        )
+      ];
+    } else if (name.contains("green")) {
+      questionAnswers = <Widget>[
+        Text(
+          'Y',
+          style: _getContentTextStyle(),
+        ),
+        Text(
+          'N',
+          style: _getContentTextStyle(),
+        )
+      ];
+    } else if (name.contains("black")) {}
+
+    return questionAnswers;
+  }
+
+
+
+  void _applyFilter() {
+
+    setState(() {
+      _strFilteredNumberPairList = [];
+    });
+
+    List<int> applyFilterArray = _questionMaker.getApplyFilterArray(_bestQuestion, _answerList);
+
+    for (int i = 0; i < applyFilterArray.length; i++) {
+      setState(() {
+        _strFilteredNumberPairList.add(_strNumberPairList[applyFilterArray[i]]);
+      });
+    }
+
+
+    if(_strFilteredNumberPairList.length == 1){
+      return;
+    }
+
+    switch(_answerList.length){
+      case 1:
+        getSecondQuestion();
+        break;
+      case 2:
+        getThirdQuestion();
+        break;
+      case 3:
+        getFourthQuestion();
+        break;
+      case 4:
+        getFifthQuestion();
+        break;
+    }
+  }
+
+
+}
